c1c181e3f44c415528024ed74abfad58781636dd
Gladys Wong <gladyswng@gmail.com>
2021-04-13 06:07:14 +0200

    input onchange mock

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 
 interface InputProps {
-
+  secretWord?: string
 }
 
-const Input: React.FC<InputProps> = ({}) => {
+const Input: React.FC<InputProps> = ({ secretWord }) => {
+  const [ currentGuess, setCurrentGuess ] = React.useState<string>()
+
   return (
-    <div data-test="component-input"></div>
+    <div data-test="component-input">
+      <form className="form-inline">
+        <input data-test="input-box" className="mb-2 mx-sm-3" type="text"
+        placeholder="enter guess"
+        value={currentGuess}
+        onChange={e => setCurrentGuess(e.target.value)}
+        />
+        <button data-test="submit-button" className="btn btn-primary mb-2">submit</button>
+      </form>
+    </div>
   )
 }
 export default Input
